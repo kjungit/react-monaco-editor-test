7eddfe5ac8776917ffe3518ec20137465073bad7
kjun <kjun_all@naver.com>
2024-01-12 20:59:30 +0900

    Feat: GuideLineCom, EditorCom 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import { Inter } from "next/font/google";
+import GuideLineCom from "../components/GuideLineCom";
+import EditorCom from "../components/EditorCom";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main className={inter.className}></main>;
+  return (
+    <main className={inter.className}>
+      <GuideLineCom />
+      <EditorCom />
+    </main>
+  );
 }
