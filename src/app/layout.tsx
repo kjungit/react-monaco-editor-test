aa7bae42f22fa43f981903f65dd955992b5bd359
kjun <kjun_all@naver.com>
2024-01-12 20:58:46 +0900

    Feat: portal 태그 추가, RecoilRootProvider 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import RecoilRootProvider from "../recoil/recoilRootProvider";
 
 export const metadata = {
   title: "Create Next App",
@@ -12,7 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children} </body>
+      <body>
+        <RecoilRootProvider>{children}</RecoilRootProvider>
+      </body>
+      <div id="portal"></div>
     </html>
   );
 }
