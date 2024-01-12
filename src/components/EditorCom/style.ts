e7e9579a4bba9de2766779ebe5a8a06b7a99d881
kjun <kjun_all@naver.com>
2024-01-12 21:13:13 +0900

    Feat: EditorCom 스타일 추가

---
+++ src/components/EditorCom/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+export const EditorWrapper = styled.div`
+  width: 50%;
+`;
+
+export const FrameWrapper = styled.div`
+  width: 50%;
+`;
